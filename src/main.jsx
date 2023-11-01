36d7050ec6f06d814f93e8bcfeb54bfab8146d81
Fazrin <fazrin.mutaqin@wgs-id.com>
2023-11-01 11:01:04 +0700

    add bootstrap css

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import { router } from './router';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
